9f6b04f94898b2ad30941b79c056c6c75a865a74
excavator <str_token@mail.ru>
2019-09-07 20:25:03 +0300

    associate the user and event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,7 +25,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.organized_events.new(event_params)
 
     respond_to do |format|
       if @event.save
